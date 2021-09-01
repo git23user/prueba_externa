d74d32e188d72e3cf5f1bb331ddfee344781ee35
inform <informinsti@gmail.com>
2021-09-01 18:58:29 +0200

    commit rama con conflicto

--- local.py
+++ local.py
@@ -1,3 +1,4 @@
-print ("Hola desde local...")
+he borrado print origianl del primer 
 print ("Hola de nuevo") //Cambio para que se vea desde diff
 print ("Modificación hecha en ramaLocal a ver que pasa después de hacer push")
+print ("Línea añadida desde Vi")
